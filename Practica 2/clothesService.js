e6c7cc2f0186e90664e6f76c156f759f6ac31936
velliscamarco <146855696+velliscamarco@users.noreply.github.com>
2023-12-03 15:02:16 +0100

    Update clothesService.js

--- Practica 2/clothesService.js
+++ Practica 2/clothesService.js
@@ -1,17 +1,20 @@
 let clothes = [
-    title = 'HODDIE 28 PROGRAM',
-    prices = 70,
-    description = 'Descubre la elegancia casual con nuestra sudadera 28 programm de la más alta calidad. Confeccionada en algodón orgánico, esta sudadera no solo es una declaración de estilo, sino también un compromiso con la sostenibilidad. fabricada en España.',
-    img = 'https://28clothing.com/cdn/shop/products/28-Evergreen-Pants-2-Front.png?v=1677854919&width=1280',
-    sizes = ['XS', 'S', 'M', 'L', 'XL'],
-    colors = ['pink'],
-
-    title = 'Pantalón 28 PROGRAM',
-    prices = 50,
-    description = 'Descubre la elegancia casual con nuestro pantalón 28 programm de la más alta calidad. Confeccionada en algodón orgánico, esta sudadera no solo es una declaración de estilo, sino también un compromiso con la sostenibilidad. Fabricado en España.',
-    img = 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/6218f8cc55984cfe92d1a96d0110ac7e_9366/Zapatilla_Handball_Spezial_Negro_DB3021_01_standard.jpg',
-    sizes = ['XS', 'S', 'M', 'L', 'XL'],
-    colors = ['white'],
+  {
+    title: 'HODDIE 28 PROGRAM',
+    price: 70,
+    description: 'Descubre la elegancia casual con nuestra sudadera 28 programm de la más alta calidad. Confeccionada en algodón orgánico, esta sudadera no solo es una declaración de estilo, sino también un compromiso con la sostenibilidad. fabricada en España.',
+    img: 'https://28clothing.com/cdn/shop/products/28-Evergreen-Pants-2-Front.png?v=1677854919&width=1280',
+    sizes: ['XS', 'S', 'M', 'L', 'XL'],
+    colors: ['pink']
+  },
+  {
+    title: 'Pantalón 28 PROGRAM',
+    price: 50,
+    description: 'Descubre la elegancia casual con nuestro pantalón 28 programm de la más alta calidad. Confeccionada en algodón orgánico, esta sudadera no solo es una declaración de estilo, sino también un compromiso con la sostenibilidad. Fabricado en España.',
+    img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/6218f8cc55984cfe92d1a96d0110ac7e_9366/Zapatilla_Handball_Spezial_Negro_DB3021_01_standard.jpg',
+    sizes: ['XS', 'S', 'M', 'L', 'XL'],
+    colors: ['white']
+  }
 
     //Hacer el resto de elemntos
 ]
