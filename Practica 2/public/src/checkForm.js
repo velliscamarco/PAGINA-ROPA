72a620cbc321681bc5cdaff2f5065f819b716975
elenxt4 <145257699+elenxt4@users.noreply.github.com>
2023-12-11 21:31:08 +0100

    Add files via upload

---
+++ Practica 2/public/src/checkForm.js
@@ -0,0 +1,16 @@
+export default function checkForm(obj) {
+    let errors = [];
+    if (obj.title.trim() === '') {
+        errors.push('El título es invalido');
+    }
+    if (!obj.img) {
+        errors.push('La imagen es obligatoria');
+    }
+    if (!obj.price) {
+        errors.push('El precio es obligatorio');
+    }
+    if (!obj.description) {
+        errors.push('La descripción es obligatoria');
+    }
+    return errors;
+}
