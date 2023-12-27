ff0665993607c327996d78dc0de68c6a24b1f97a
elenxt4 <145257699+elenxt4@users.noreply.github.com>
2023-12-27 12:26:17 +0100

    Add files via upload

--- Practica 3/public/app.js
+++ Practica 3/public/app.js
@@ -2,23 +2,25 @@ const NUM_RESULTS = 3;
 
 let loadMoreRequests = 0;
 
+//Cargar mas prendas (hay que adaptarlo a la pagina de ropa)
 async function loadMore() {
 
     const from = (loadMoreRequests + 1) * NUM_RESULTS;
     const to = from + NUM_RESULTS;
 
-    const response = await fetch(`/superheroes?from=${from}&to=${to}`);
+    const response = await fetch(`/clothes?from=${from}&to=${to}`);
 
     const newClothe = await response.text();
 
     const ClotheDiv = document.getElementById("Clothe");
 
-    ClotheDiv.innerHTML += newSuperheroes;
+    ClotheDiv.innerHTML += newClothess;
 
     loadMoreRequests++;
 }
 
 
+//Validar formulario, hacer con las maysuculas del titulo (y con el resto de apartados)
 async function checktitle() {
 
     let titleInput = document.getElementById('title');
