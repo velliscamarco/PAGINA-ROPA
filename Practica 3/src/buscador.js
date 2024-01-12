da54f8ae45f13bba16f4176858afa23e56586037
elenxt4 <145257699+elenxt4@users.noreply.github.com>
2024-01-12 20:42:28 +0100

    Add files via upload

--- Practica 3/src/buscador.js
+++ Practica 3/src/buscador.js
@@ -1,27 +1,35 @@
-// Js encargado de filtrar los productos por nombre 
-const formulario = document.querySelector('#formulario');
-const boton = document.querySelector('#boton');
-const resultado = document.querySelector('#resultado');
-
-const filtrar = () => {
-    resultado.innerHTML = ''; //vaciamos el resultado para que no se acumulen los resultados de busquedas anteriores
-    const texto = formulario.value.toLowerCase(); //pasamos el texto a minusculas
-    for (let producto of productos) {
-        let title = producto.title.toLowerCase(); //pasamos el nombre del producto a minusculas   
-        if (nombre.indexOf(texto) !== -1) { //si el nombre del producto incluye el texto que el usuario ha escrito por pantalla
-            resultado.innerHTML += `<li>${producto.title} - img: ${producto.img}</li>`; //añadimos el producto a la lista de resultados
-        }
-    }
-    if (resultado.innerHTML === '') { //si no hay resultados
-        resultado.innerHTML += `<li>Producto no encontrado...</li>`; //añadimos un mensaje de que no hay resultados
-    }
-}
-
-boton.addEventListener('click', filtrar); //cuando el usuario haga click en el boton se ejecuta la funcion filtrar
-formulario.addEventListener('keyup', filtrar); //cuando el usuario suelte una tecla se ejecuta la funcion filtrar
-filtrar();
 //Productos de ejemplo
-const productos = [
-    { title: 'HODDIE 28 PROGRAM', img: 'https://28clothing.com/cdn/shop/products/PINKHOODIE-2.png?v=1693234992&width=1280' },
-    { title: 'Pantalón 28 PROGRAM', img: 'https://28clothing.com/cdn/shop/products/28-Evergreen-Pants-2-Front.png?v=1677854919&width=1280' },
+const products = [
+    { name: 'HODDIE 28 PROGRAM' },
+    { name: 'Pantalón 28 PROGRAM' },
 ]
+
+const searchInput = document.getElementById('searchInput'); //obtenemos el input de busqueda
+const resultsList = document.getElementById('resultList'); //obtenemos la lista de resultados
+const noResults = document.getElementById('noResults'); //obtenemos el mensaje de no resultados
+
+
+const handleSearch = () => {
+    const searchItem = searchInput.value.toLowerCase(); //obtenemos el texto de busqueda y lo pasamos a minusculas
+    const filteredProducts = products.filter8((products) => products.name.toLowerCase().startsWith(searchItem)); //filtramos los productos
+
+    resultsList.innerHTML = ''; //vaciamos la lista de resultados
+
+    if (filteredProducts.length === 0) { //si no hay resultados
+        noResults.style.display = "block";
+    } else { //mostramos el mensaje de no resultados
+        filteredProducts.forEach((products) => { //por cada producto filtrado    
+            const li = document.createElement('li'); //creamos un elemento li
+            li.textContent = products.name; //le añadimos el nombre del producto
+            resultsList.appendChild(li); //añadimos el elemento li a la lista de resultados
+        });
+        noResults.style.display = "none"; //ocultamos el mensaje de no resultados
+    }
+
+    if (searchInput.value === '') { //si el input de busqueda esta vacio
+        resultsList.innerHTML = ''; //vaciamos la lista de resultados
+    }
+};
+
+searchInput.addEventListener('input', handleSearch); //añadimos un listener al input de busqueda
+
